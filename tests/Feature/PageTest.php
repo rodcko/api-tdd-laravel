154c574643bd5e3b59716b2ea75011f97d9e4f29
Rodrigo Hernández <42217144+rodcko@users.noreply.github.com>
2021-03-28 12:23:16 -0600

    Metodologia TDD y testing HTTP

--- tests/Feature/PageTest.php
+++ tests/Feature/PageTest.php
@@ -6,16 +6,16 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class UserTest extends TestCase
+class PageTest extends TestCase
 {
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    public function test_about()
     {
-        $response = $this->get('/');
+        $response = $this->get('/about');
 
         $response->assertStatus(200);
     }
